4762090845470248e57775514fd2a11b97062954
infinityshades <tash.aneiq@gmail.com>
2020-08-03 19:39:58 +0200

    Created Auth Pages and Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./pages/Auth.jsx";
+import BookingsPage from "./pages/Bookings.jsx";
+import EventsPage from "./pages/Events.jsx";
+
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainNavigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" to="/auth" exact></Redirect>
+          <Route path="/auth" component={AuthPage}></Route>
+          <Route path="/bookings" component={BookingsPage}></Route>
+          <Route path="/events" component={EventsPage}></Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import "./Auth.css";
+
+export default class AuthPage extends Component {
+  state = {
+    isLogin: true,
+  };
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  switchHandler = (e) => {
+    e.preventDefault();
+    this.setState({ isLogin: !this.state.isLogin });
+  };
+
+  submitHandler = async (e) => {
+    e.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let payload = {
+      query: `
+      query{
+        login(email:"${email}", password:"${password}"){
+          userId
+          token
+          tokenExpiration
+        }
+      }
+      `,
+    };
+
+    if (!this.state.isLogin) {
+      payload = {
+        query: `
+        mutation{
+          createUser(userInput:{email:"${email}", password:"${password}"}){
+            _id
+            email
+            password
+            createdEvents{
+              title
+            }
+          }
+        }
+        `,
+      };
+    }
+
+    console.log(email, password);
+    try {
+      const submitURL = await fetch("http://localhost:3000/graphql", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (submitURL.ok) {
+        const responseJson = await submitURL.json();
+        console.log(responseJson);
+      }
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Auth Page</h1>
+        <form className="auth-form">
+          <div className="form-control">
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" ref={this.emailEl} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" ref={this.passwordEl} />
+          </div>
+          <div className="form-actions">
+            <button onClick={this.submitHandler}>
+              {this.state.isLogin ? "Sign In" : "Create Account"}
+            </button>
+            <button onClick={this.switchHandler}>
+              Switch to {this.state.isLogin ? "Signup" : "Login"}
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
